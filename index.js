32c49de385ea58c76222bc9a2f7bfa69a9fdcbb2
[Sohail khan] <[sohail841khan@gmail.com]>
2026-01-02 13:38:36 +0530

    updated code

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const v = 1;
 const b = 2;
+
+function mynewfunction(params) {
+    return params;
+}
+mynewfunction("name");
--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 var name = sohail;
+
+async function newfunctions(params) {
+    var a = 10;
+    var b = 20;
+
+    let total = a + b;
+
+    return total;
+
+}
+
+newfunctions();
